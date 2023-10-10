6bf6920ec61074566cdfb52005a3eaeb52bb594b
farshadgh90 <f.ghorbani1990@gmail.com>
2023-10-10 13:31:09 +0330

    Delete notification

---
+++ src/features/categories/components/add-or-update-category.jsx
@@ -0,0 +1,7 @@
+const AddOrUpdateCategory = ({setShowAddCategory}) => {
+    return (
+        <h2>add ou update category</h2>
+    )
+}
+
+export default AddOrUpdateCategory
--- src/pages/course-categories.jsx
+++ src/pages/course-categories.jsx
@@ -3,26 +3,51 @@ import { httpInterceptedService } from "@core/http-service";
 import { Suspense, useState } from "react";
 import CategoryList from "../features/categories/components/category-list";
 import Modal from "../components/modal";
+import { toast } from "react-toastify";
+import { useTranslation } from "react-i18next";
 
 const CourseCategories = () => {
   const data = useLoaderData();
-  const [showDeleteModal, setShowDeleteModal] = useState(false)
-  const [selectedCategory, setSelectedCategory] = useState()
-  const navigate = useNavigate()
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [showAddCategory, setShowAddCategory] = useState();
+
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const deleteCategory = (categoryId) => {
-    setSelectedCategory(categoryId)
-    setShowDeleteModal(true)
-  }
+    setSelectedCategory(categoryId);
+    setShowDeleteModal(true);
+  };
 
   const handleDeleteCategory = async () => {
-    setShowDeleteModal(false)
-    const response = await httpInterceptedService.delete(`/CourseCategory/${selectedCategory}`)
-    if(response.status === 200) {
-      const url = new URL(window.location.href)
-      navigate(url.pathname + url.search)
-    }
-  }
+    setShowDeleteModal(false);
+    const response = httpInterceptedService.delete(
+      `/CourseCategory/${selectedCategory}`
+    );
+
+    toast.promise(
+      response,
+      {
+        pending: "در حال حذف...",
+        success: {
+          render() {
+            const url = new URL(window.location.href);
+            navigate(url.pathname + url.search);
+            return "عملیات با موفقیت انجام شد";
+          },
+        },
+        error: {
+          render({ data }) {
+            return t("categoryList." + data.response.data.code);
+          },
+        },
+      },
+      {
+        position: toast.POSITION.BOTTOM_LEFT,
+      }
+    );
+  };
 
   return (
     <>
@@ -36,15 +61,35 @@ const CourseCategories = () => {
           >
             <Await resolve={data?.categories}>
               {(loadedCategories) => (
-                <CategoryList deleteCategory={deleteCategory} categories={loadedCategories} />
+                <CategoryList
+                  deleteCategory={deleteCategory}
+                  categories={loadedCategories}
+                />
               )}
             </Await>
           </Suspense>
         </div>
       </div>
-      <Modal isOpen={showDeleteModal} open={setShowDeleteModal} title="حذف" body="آیا از حذف این دسته اطمینان دارید؟">
-        <button type="button" className="btn btn-secondary fw-bolder" onClick={() => setShowDeleteModal(false)}>انصراف</button>
-        <button type="button" className="btn btn-primary fw-bolder" onClick={handleDeleteCategory}>حذف</button>
+      <Modal
+        isOpen={showDeleteModal}
+        open={setShowDeleteModal}
+        title="حذف"
+        body="آیا از حذف این دسته اطمینان دارید؟"
+      >
+        <button
+          type="button"
+          className="btn btn-secondary fw-bolder"
+          onClick={() => setShowDeleteModal(false)}
+        >
+          انصراف
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary fw-bolder"
+          onClick={handleDeleteCategory}
+        >
+          حذف
+        </button>
       </Modal>
     </>
   );

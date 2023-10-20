7dc1efda7f8b48b6b12fb0046631975021b64420
farshadgh90 <f.ghorbani1990@gmail.com>
2023-10-20 19:10:53 +0330

    translate

--- src/layouts/mainLayout/top-nav.jsx
+++ src/layouts/mainLayout/top-nav.jsx
@@ -1,11 +1,19 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ChangeLanguage from "../../components/change-language";
 import ChangeTheme from "../../components/change-theme";
 import { useAppContext } from "../../contexts/app/app-context";
+import { useTranslation } from "react-i18next";
 
 const TopNav = () => {
     const {toggleSidebar} = useAppContext();
     const {language} = useAppContext();
+    const navigate = useNavigate()
+    const {t} = useTranslation()
+
+    const logout = () => {
+      localStorage.removeItem('token')
+      navigate('/login')
+    }
 
     return (
         <nav className="navbar">
@@ -20,8 +28,8 @@ const TopNav = () => {
             <ChangeTheme />
           </div>
           <div className={`${language === 'fa' ? 'me-auto' : 'ms-auto'}`}>
-            <button className="btn ms-2 btn-outline-danger fw-bolder">
-              خارج شوید
+            <button className="btn ms-2 btn-outline-danger fw-bolder" onClick={logout}>
+            {t("mainLayout.topSidebar.logout")}
             </button>
           </div>
         </nav>
--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -2,18 +2,22 @@ import { httpInterceptedService } from "@core/http-service";
 import CourseList from "../features/courses/components/course-list";
 import { Await, defer, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 
 const Courses = () => {
   const data = useLoaderData();
+  const { t } = useTranslation();
 
   return (
     <div className="row">
       <div className="col-12">
         <div className="d-flex align-items-center justify-content-between mb-5">
-          <a className="btn btn-primary fw-bolder mt-n1">افزودن دوره جدید</a>
+          <a className="btn btn-primary fw-bolder mt-n1">
+          {t("mainLayout.newCourse")}
+          </a>
         </div>
         <Suspense
-          fallback={<p className="text-info">در حال دریافت اطلاعات...</p>}
+          fallback={<p className="text-info">{t('mainLayout.loading')}</p>}
         >
           <Await resolve={data.courses}>
             {(loadedCourses) => <CourseList courses={loadedCourses} />}
